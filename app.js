8ad28a834b91c8ca5ffde6f30ea4e2af9659a987
Amar Phule <amardeepphule@gmail.com>
2021-09-23 09:04:03 +0530

    fix: profit-loss calculation

--- app.js
+++ app.js
@@ -4,27 +4,29 @@ const currentPrice = document.querySelector("#current-price")
 const submit = document.querySelector("#submit")
 const result = document.querySelector("#result")
 
+function message(msg) {
+    result.innerText = msg
+}
+
 function checkProfitLoss() {
     const intial = initialPrice.value;
     const quantity = stockQuantity.value;
     const current = currentPrice.value;
 
-    function message(msg) {
-        result.innerText = msg
-    }
-
-    if (intial && quantity && current) {
+    if (intial > 0 && quantity > 0 && current > 0) {
         if (current > intial) {
-            const profit = current - intial
+            const profit = (current - intial) * quantity
 
-            const profitPercent = ((profit / current) * 100) * quantity
+            const profitPercent = (profit / intial) * 100
+            result.style.color = "green"
             let output = `Hey, the profit is ${profit} and the percent is ${profitPercent.toFixed(2)}%`
             message(output)
 
         } else if (intial > current) {
             const loss = intial - current
-            const lossPercent = ((loss / current) * 100) * quantity
-            let output = `Hey, the loss is ${loss} and the percent is ${lossPercent.toFixed(2)}%`
+            const lossPercent = ((loss / intial) * 100) * quantity
+            result.style.color = "red"
+            let output = `Hey, the loss is ${loss} and the percent is -${lossPercent.toFixed(2)}%`
             message(output)
 
         } else {
@@ -32,7 +34,7 @@ function checkProfitLoss() {
 
         }
     } else {
-        message("Please insert numbers")
+        message("Please insert valid numbers")
     }
 }
 
